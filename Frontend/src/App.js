1f3f37567245d1dbc87dcfb4dbd28a5efd96b442
Rashid-F-Walcott <Rashidwalcott@gmail.com>
2021-11-29 09:49:47 +0000

    added homepage not complete

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,11 @@
 import GameBoard from "./containers/GameBoard";
+import Homepage from "./containers/Homepage";
 
 function App() {
   return (
     <>
-      <GameBoard />
-      <h1>Hello</h1>
+      {/* <GameBoard /> */}
+      <Homepage />
     </>
   );
 }
---
+++ Frontend/src/containers/Homepage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Homepage = () => {
+
+return (
+<>
+<span id="bannerPic">
+<div id="homepageBanner">
+</div>
+<h1 className="Mainheading">Welcome to DnD Spellbook</h1>
+</span>
+<div id="wrapper">
+    <div className="MainBoxContainer">
+        <div className="mainboxSpells">
+           <span className="mainboxSpellsText">View All Spells </span>
+           </div>
+        <div className="mainboxGame">
+            <span className="mainboxGameText"> Track your Game </span> </div>
+    </div></div>
+    </>
+
+)}
+ export default Homepage;
