30b019f4f561ccc52c76a396f815e25f5591ea28
Vinh Chu <vinhchugg@gmail.com>
2021-11-30 12:36:21 +0100

    mistakes

--- Frontend/src/components/NewSpellForm.js
+++ Frontend/src/components/NewSpellForm.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const NewSpellForm = ({onSpellSubmission}) => {
 
     const [spellName, setSpellName] = useState("");
-    const [spellLevel, setSpellLevel] = useState(null);
+    const [spellLevel, setSpellLevel] = useState();
     const [range, setRange] = useState("");
     const [components, setComponents] = useState("");
     const [duration, setDuration] = useState("");
@@ -18,7 +18,7 @@ const NewSpellForm = ({onSpellSubmission}) => {
     const [canDruid, setCanDruid] = useState(false);
     const [canCleric, setCanCleric] = useState(false);
     const [canRanger, setCanRanger] = useState(false);
-    const [schoolID, setSchoolID] = useState([]);
+    const [schoolID, setSchoolID] = useState();
 
     const handleSpellNameChange = (event) => {
         setSpellName(event.target.value);
@@ -52,21 +52,85 @@ const NewSpellForm = ({onSpellSubmission}) => {
         setRitual(value => !value);
     }
 
+    const toggleSorcerer = () => {
+        setCanSorcerer(value => !value);
+    }
+
+    const toggleWizard = () => {
+        setCanWizard(value => !value);
+    }
+
+    const toggleWarlock = () => {
+        setCanWarlock(value => !value);
+    }
+
+    const toggleBard = () => {
+        setCanBard(value => !value);
+    }
+
+    const togglePaladin = () => {
+        setCanPaladin(value => !value);
+    }
+
+    const toggleDruid = () => {
+        setCanDruid(value => !value);
+    }
+
+    const toggleCleric = () => {
+        setCanCleric(value => !value);
+    }
+
+    const toggleRanger = () => {
+        setCanRanger(value => !value);
+    }
+
+    const handleSchoolIDChange = (event) => {
+        setSchoolID(event.target.value);
+    }
+
+
     const handleFormSubmission = (event) => {
         event.preventDefault();
     
-
         const newSpell = {
             spellName: spellName,
             spellLevel: spellLevel,
-            range: range
+            range: range,
+            components: components,
+            duration: duration,
+            description: description,
+            higherLevel: higherLevel,
+            ritual: ritual,
+            canSorcerer: canSorcerer,
+            canWizard: canWizard,
+            canWarlock: canWarlock,
+            canBard: canBard,
+            canPaladin: canPaladin,
+            canDruid: canDruid,
+            canCleric: canCleric,
+            canRanger: canRanger,
+            schoolID: schoolID
         }
 
         onSpellSubmission(newSpell);
 
         setSpellName("");
-        setSpellLevel(null);
+        setSpellLevel();
         setRange("");
+        setComponents("");
+        setDuration("");
+        setDescription("");
+        setHigherLevel("");
+        setRitual(false);
+        setCanSorcerer(false);
+        setCanWizard(false);
+        setCanWarlock(false);
+        setCanBard(false);
+        setCanPaladin(false);
+        setCanDruid(false);
+        setCanCleric(false);
+        setCanRanger(false);
+        setSchoolID();
     }
 
     return (
@@ -109,6 +173,52 @@ const NewSpellForm = ({onSpellSubmission}) => {
                     <label htmlFor="spell-ritual">Ritual: </label>
                     <input type="checkbox" id="spell-ritual" value={ritual} onChange={toggleRitual}/>
                 </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-sorcerer">Sorcerer: </label>
+                    <input type="checkbox" id="spell-sorcerer" value={canSorcerer} onChange={toggleSorcerer}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-wizard">Wizard: </label>
+                    <input type="checkbox" id="spell-wizard" value={canWizard} onChange={toggleWizard}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-warlock">Warlock: </label>
+                    <input type="checkbox" id="spell-warlock" value={canWarlock} onChange={toggleWarlock}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-bard">Bard: </label>
+                    <input type="checkbox" id="spell-bard" value={canBard} onChange={toggleBard}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-paladin">Paladin: </label>
+                    <input type="checkbox" id="spell-paladin" value={canPaladin} onChange={togglePaladin}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-druid">Druid: </label>
+                    <input type="checkbox" id="spell-druid" value={canDruid} onChange={toggleDruid}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-cleric">Cleric: </label>
+                    <input type="checkbox" id="spell-cleric" value={canCleric} onChange={toggleCleric}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-ranger">Ranger: </label>
+                    <input type="checkbox" id="spell-ranger" value={canRanger} onChange={toggleRanger}/>
+                </div>
+
+                <div className="formElement">
+                    <label htmlFor="spell-school-id">School: </label>
+                    <input type="number" id="spell-school-id" value={schoolID} onChange={handleSchoolIDChange}/>
+                </div>
+
 
                 <div className="formElement">
                     <input type="submit" value="Submit Spell" />
--- Frontend/src/containers/GameBoard.js
+++ Frontend/src/containers/GameBoard.js
@@ -1,17 +1,17 @@
 import {useState, useEffect} from 'react';
-import WizardDetails from '../Testing/WizardDetails';
 import SpellDetails from '../components/SpellDetails';
-import NewSpellForm from '../components/NewSpellForm';
 
 const GameBoard = () => {
 
   const [spells, setSpells] = useState([]);
 
-  useEffect(() => {
+  const getSpellData = () => {
     fetch("http://localhost:8080/api/v1/spells")
     .then(response => response.json())
     .then(data => setSpells(data));
-  }, [])
+  }
+
+  useEffect(getSpellData, [])
 
   const [search, setSearch] = useState(""); 
 
@@ -23,24 +23,9 @@ const GameBoard = () => {
     return spells.filter((spell) => spell.spellName.toLowerCase().indexOf(search) > -1); 
 }
 
-const addNewSpell = (newSpell) => {
-  fetch("http://localhost:8080/api/v1/spells", {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newSpell)
-  })
-      .then(
-        fetch("http://localhost:8080/api/v1/spells")
-        .then(response => response.json())
-        .then(data => setSpells(data)));
-}
-
 
     return (
           <>
-          <NewSpellForm onSpellSubmission={addNewSpell} />
           <SpellDetails spells={searching(spells)} search={search} handleSearching={handleSearching} />
           </>
         );
